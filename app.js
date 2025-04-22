ea826c30b1ddc7a8052a0a5ba9f8658aa0ab2ae2
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2025-04-22 14:05:46 -0500

    fdaf

--- app.js
+++ app.js
@@ -1,162 +1,162 @@
 
 
-//functions can be called before initialization
-//funcOne();
+// //functions can be called before initialization
+// //funcOne();
 
-//function schema
-//making functions saves blocks of code to be ran when we need it.
-//INSTEAD of immediately running as the page loads
-function funcOne() {
-    console.log("Hello World!");
-    console.log(5 + 7);
-};
-//This is how we call function code blocks to run
-//ex. funcName(); 
-funcOne();
+// //function schema
+// //making functions saves blocks of code to be ran when we need it.
+// //INSTEAD of immediately running as the page loads
+// function funcOne() {
+//     console.log("Hello World!");
+//     console.log(5 + 7);
+// };
+// //This is how we call function code blocks to run
+// //ex. funcName(); 
+// funcOne();
 
-let numOne = 7;
-let numTwo = 3;
-
-
-//Scope: whether it's GLOBAL, BLOCK, FUNCTION SCOPE(Local scope)
-function addTwoNums() {
-    console.log(numOne + numTwo);
-}
+// let numOne = 7;
+// let numTwo = 3;
 
 
-addTwoNums();
-
-//THis function is currently accepting two arguments (args)
-function addTwoNumsV2(a,b) {
-    console.log(a + b);
-}
-
-//We are sending data inside the arguments spots in functions;
-addTwoNumsV2(numOne, numTwo);
-addTwoNumsV2(6, 10);
+// //Scope: whether it's GLOBAL, BLOCK, FUNCTION SCOPE(Local scope)
+// function addTwoNums() {
+//     console.log(numOne + numTwo);
+// }
 
 
-function greeting() {
-    console.log("HI");
-}
-greeting();
+// addTwoNums();
+
+// //THis function is currently accepting two arguments (args)
+// function addTwoNumsV2(a,b) {
+//     console.log(a + b);
+// }
+
+// //We are sending data inside the arguments spots in functions;
+// addTwoNumsV2(numOne, numTwo);
+// addTwoNumsV2(6, 10);
+
+
+// function greeting() {
+//     console.log("HI");
+// }
+// greeting();
    
-function greetingByName(name) {
-    console.log("Hello, " + name + "! Welcome to the site.");
-}
-greetingByName("Dayna");
+// function greetingByName(name) {
+//     console.log("Hello, " + name + "! Welcome to the site.");
+// }
+// greetingByName("Dayna");
 
-greetingByName("Bryan");
+// greetingByName("Bryan");
 
 
-function greetingByExcitment(name) {
-    //THIS let variable is only being seen inside this function!
-    let upperCaseName = name.toUpperCase();
-    console.log(upperCaseName);
+// function greetingByExcitment(name) {
+//     //THIS let variable is only being seen inside this function!
+//     let upperCaseName = name.toUpperCase();
+//     console.log(upperCaseName);
     
-    //THIS IS Sending back the output of this function
-    return upperCaseName
-}
+//     //THIS IS Sending back the output of this function
+//     return upperCaseName
+// }
    
 
-//IF you ever need a function to send back new data, You'll need a return
-let screamingName = greetingByExcitment("zach");
+// //IF you ever need a function to send back new data, You'll need a return
+// let screamingName = greetingByExcitment("zach");
 
-// upperCaseName
-//console.log(upperCaseName); //! ERRORED OUT bc Local scoped to "greetingByExcitment() function"
-console.log(`HELLLLOOOOOO ${screamingName} IT'S NORTHERN LIGHT SUPLEX SINGER!!`);
+// // upperCaseName
+// //console.log(upperCaseName); //! ERRORED OUT bc Local scoped to "greetingByExcitment() function"
+// console.log(`HELLLLOOOOOO ${screamingName} IT'S NORTHERN LIGHT SUPLEX SINGER!!`);
 
-//THIS functions shows DEFAULT values being added in the Arguments
-function sandwichMaker(typeOfBread, typeOfMeat = "N/A", typeOfSauce, typeOfVeggie = "N/A") {
-    console.log(
-        `Bread: ${typeOfBread} Meat: ${typeOfMeat} Sauce: ${typeOfSauce} Veggies: ${typeOfVeggie}`
-    )
-}
+// //THIS functions shows DEFAULT values being added in the Arguments
+// function sandwichMaker(typeOfBread, typeOfMeat = "N/A", typeOfSauce, typeOfVeggie = "N/A") {
+//     console.log(
+//         `Bread: ${typeOfBread} Meat: ${typeOfMeat} Sauce: ${typeOfSauce} Veggies: ${typeOfVeggie}`
+//     )
+// }
 
-let zachSandwich = sandwichMaker("white", "ham", "mayo", "lettuce");
-let margaritoSandwich = sandwichMaker("white",undefined, "buffalo", "lettuce")
-
-
+// let zachSandwich = sandwichMaker("white", "ham", "mayo", "lettuce");
+// let margaritoSandwich = sandwichMaker("white",undefined, "buffalo", "lettuce")
 
 
-//! Grade Calculator
-//? Create a program that calculates the average grade from an array of grades and determines the corresponding letter grade.
 
-//TODO Define an Array: Define an array containing a list of 5 grades.
-let grades = [85,34,56,87,90];
-let gradeTwo = [82,56,31,78,67];
-let currentAverage = calcAverage(grades);
 
-//TODO Create a Function: Create a function to calculate the average of the grades.
-//? calculates the average grade from an array of grades
-function calcAverage(gradesArr) {
-    let average = 0;
-    for (let i = 0; i < gradesArr.length; i++) {
-        average += gradesArr[i]
+// //! Grade Calculator
+// //? Create a program that calculates the average grade from an array of grades and determines the corresponding letter grade.
+
+// //TODO Define an Array: Define an array containing a list of 5 grades.
+// let grades = [85,34,56,87,90];
+// let gradeTwo = [82,56,31,78,67];
+// let currentAverage = calcAverage(grades);
+
+// //TODO Create a Function: Create a function to calculate the average of the grades.
+// //? calculates the average grade from an array of grades
+// function calcAverage(gradesArr) {
+//     let average = 0;
+//     for (let i = 0; i < gradesArr.length; i++) {
+//         average += gradesArr[i]
         
-    }
-    average = average / gradesArr.length;
-    return average
-}
+//     }
+//     average = average / gradesArr.length;
+//     return average
+// }
 
-console.log(currentAverage)
+// console.log(currentAverage)
 
-//TODO Create Another Function: Create another function to determine the letter grade based on the average.
+// //TODO Create Another Function: Create another function to determine the letter grade based on the average.
 
-function letterGrade(gradeNum) {
-    if (gradeNum > 100) {
-        console.log(`${gradeNum} can't exist and recieve no letter!`)
-    } else if (gradeNum >= 90) {
-        console.log(`${gradeNum} has a A average!`)
-    } else if (gradeNum >= 80) {
-        console.log(`${gradeNum} has a B average!`)
-    } else if (gradeNum >= 70) {
-        console.log(`${gradeNum} has a C average!`)
-    } else if ( gradeNum >= 60 ) {
-        console.log(`${gradeNum} has a D average!`)
-    } else {
-        console.log(`${gradeNum} has a F average!`)
+// function letterGrade(gradeNum) {
+//     if (gradeNum > 100) {
+//         console.log(`${gradeNum} can't exist and recieve no letter!`)
+//     } else if (gradeNum >= 90) {
+//         console.log(`${gradeNum} has a A average!`)
+//     } else if (gradeNum >= 80) {
+//         console.log(`${gradeNum} has a B average!`)
+//     } else if (gradeNum >= 70) {
+//         console.log(`${gradeNum} has a C average!`)
+//     } else if ( gradeNum >= 60 ) {
+//         console.log(`${gradeNum} has a D average!`)
+//     } else {
+//         console.log(`${gradeNum} has a F average!`)
 
-    }
-}
+//     }
+// }
 
-letterGrade(currentAverage)
-letterGrade();
-//TODO Use if-else Statements: Use if-else statements inside the function to determine the letter grade.
-//* Output Results: Use console.log to display the average grade and the corresponding letter grade. 
-
-
+// letterGrade(currentAverage)
+// letterGrade();
+// //TODO Use if-else Statements: Use if-else statements inside the function to determine the letter grade.
+// //* Output Results: Use console.log to display the average grade and the corresponding letter grade. 
 
 
-//! Simple To-Do List
-//? Create a program that allows the user to add and display tasks in a to-do list.
 
-//TODO Define an Array: Define an array to store the tasks.
-//TODO Create Functions: Create functions to add a task and display the to-do list.
 
-function addATask(additionalTask) {
-    //how do i add an item to the array?
-    taskToDo.push(additionalTask);
-    displayToDoList();
+// //! Simple To-Do List
+// //? Create a program that allows the user to add and display tasks in a to-do list.
+
+// //TODO Define an Array: Define an array to store the tasks.
+// //TODO Create Functions: Create functions to add a task and display the to-do list.
+
+// function addATask(additionalTask) {
+//     //how do i add an item to the array?
+//     taskToDo.push(additionalTask);
+//     displayToDoList();
     
-}
+// }
 
-function displayToDoList() {
-    console.log(taskToDo);
+// function displayToDoList() {
+//     console.log(taskToDo);
     
-}
+// }
 
-let taskToDo = [];
-                    //!taskcompleted = "make food!"
-function removeAnItem(taskCompleted) {
-    //I need to remove a specific item.
-    //If else statements? 
-    // if (taskCompleted == "Make bed") {
-    //     console.log("Its here!")
-    // }
+// let taskToDo = [];
+//                     //!taskcompleted = "make food!"
+// function removeAnItem(taskCompleted) {
+//     //I need to remove a specific item.
+//     //If else statements? 
+//     // if (taskCompleted == "Make bed") {
+//     //     console.log("Its here!")
+//     // }
     
-    let uppercaseTaskCompleted = taskCompleted.toUpperCase();
-    //console.log(uppercaseTaskCompleted)
+//     let uppercaseTaskCompleted = taskCompleted.toUpperCase();
+//     //console.log(uppercaseTaskCompleted)
 
 
 
@@ -165,109 +165,109 @@ function removeAnItem(taskCompleted) {
 
 
 
-    console.log(taskCompleted); //String from function arguement 
-    console.log(taskToDo) //array from global variable ;
-                        //taskToDo = ARRAY OF DATA []
-    // if (taskCompleted === taskToDo[0]) {
-    //     taskToDo.splice(0,1);
-    // } else if (taskCompleted === taskToDo[1]) {
-    //     taskToDo.splice(1,1);
-    // } else if( taskCompleted === taskToDo[2]) {
-    //     taskToDo.splice(2,1)
-    // }
-    for (let i = 0; i < taskToDo.length; i++) {
-        if (taskCompleted === taskToDo[i]) {
-            taskToDo.splice(i,1);
-            displayToDoList();
-        }
-    }
-}
+//     console.log(taskCompleted); //String from function arguement 
+//     console.log(taskToDo) //array from global variable ;
+//                         //taskToDo = ARRAY OF DATA []
+//     // if (taskCompleted === taskToDo[0]) {
+//     //     taskToDo.splice(0,1);
+//     // } else if (taskCompleted === taskToDo[1]) {
+//     //     taskToDo.splice(1,1);
+//     // } else if( taskCompleted === taskToDo[2]) {
+//     //     taskToDo.splice(2,1)
+//     // }
+//     for (let i = 0; i < taskToDo.length; i++) {
+//         if (taskCompleted === taskToDo[i]) {
+//             taskToDo.splice(i,1);
+//             displayToDoList();
+//         }
+//     }
+// }
 
 
-addATask("Make bed") //[0]
-addATask("Code some") //[1]
-addATask("Make food!") //[2]
-removeAnItem("make food!")
+// addATask("Make bed") //[0]
+// addATask("Code some") //[1]
+// addATask("Make food!") //[2]
+// removeAnItem("make food!")
 
 
-//TODO Use if-else Statements and Loops: Use if-else statements and loops within the functions to handle the tasks.
-//* Output Results: Use console.log to display the to-do list after each operation.
+// //TODO Use if-else Statements and Loops: Use if-else statements and loops within the functions to handle the tasks.
+// //* Output Results: Use console.log to display the to-do list after each operation.
 
-//! BONUS: Be able to remove a task o.o 
-
-
+// //! BONUS: Be able to remove a task o.o 
 
 
-//! Temperature Converter
-//? Create a program that converts temperatures between Celsius, Fahrenheit, and Kelvin.
-
-//TODO Define Variables: Allow the user to input a temperature value and the unit they want to convert from.
-let tempNum = 25;
-let tempUnitLetter = "k";
-
-let newTempNum = 0;
-
-//TODO Create Functions: Create functions to convert between Celsius, Fahrenheit, and Kelvin.
-
-//?Making this function purely for changing Celsius number to Kelvin Number!
-function celsiusToKelvin(value) {
-
-    //*I KNOW I'M to have to store this kelvin number somewhere.
-    //*Planning to return to a store variable.
-    return value + 273.15;
-}
-
-function celsiusToFahrenheit(value) {
-    return (value * 9/5) + 32 
-}
-
-function KelvinToCelsius(value) {
-    return value - 273.15
-}
-
-function fahrenheitToCelsius(value) {
-    return (value - 32) * 5/9
-}
 
 
-// let storedKelvinNum = celsiusToKelvin(tempNum); //!WORKS
-// console.log(storedKelvinNum) //! 298.15 degrees
-// let storedFahrenheitNum = celsiusToFahrenheit(tempNum); //!WORKS
-// console.log(storedFahrenheitNum); //! 77 degrees
+// //! Temperature Converter
+// //? Create a program that converts temperatures between Celsius, Fahrenheit, and Kelvin.
 
-//? I have the conversions when dealing with celsius numbers
-//TODO I NEED a way to tell apart and give me the other degrees
+// //TODO Define Variables: Allow the user to input a temperature value and the unit they want to convert from.
+// let tempNum = 25;
+// let tempUnitLetter = "k";
 
-function tempConverter(currentTempValue, currentTempLetter) {
-    console.log(currentTempValue + " is the tempNum");
-    console.log(currentTempLetter + " is the tempUnitLetter");
-    //MADE THIS VARIABLE just in case a user inputs a lowercase letter.
-    let upperCaseLetter = currentTempLetter.toUpperCase();
-    console.log(upperCaseLetter)
-    let celsius = 0;
-    let fahrenheit = 0;
-    let kelvin = 0;
+// let newTempNum = 0;
 
-    if (upperCaseLetter === "C") {
-        fahrenheit = celsiusToFahrenheit(currentTempValue);
-        kelvin = celsiusToKelvin(currentTempValue);
-        celsius = currentTempValue;
-        console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
-    } else if (upperCaseLetter === "F") {
-        fahrenheit = currentTempValue;
-        celsius = fahrenheitToCelsius(currentTempValue);
-        kelvin = celsiusToKelvin(celsius);
-        console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
-    } else if (upperCaseLetter === "K") {
-        celsius = KelvinToCelsius(currentTempValue);
-        fahrenheit = celsiusToFahrenheit(celsius);
-        kelvin = currentTempValue;
-        console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
-    }
-}
+// //TODO Create Functions: Create functions to convert between Celsius, Fahrenheit, and Kelvin.
+
+// //?Making this function purely for changing Celsius number to Kelvin Number!
+// function celsiusToKelvin(value) {
+
+//     //*I KNOW I'M to have to store this kelvin number somewhere.
+//     //*Planning to return to a store variable.
+//     return value + 273.15;
+// }
+
+// function celsiusToFahrenheit(value) {
+//     return (value * 9/5) + 32 
+// }
+
+// function KelvinToCelsius(value) {
+//     return value - 273.15
+// }
+
+// function fahrenheitToCelsius(value) {
+//     return (value - 32) * 5/9
+// }
 
 
-tempConverter(tempNum, tempUnitLetter)
+// // let storedKelvinNum = celsiusToKelvin(tempNum); //!WORKS
+// // console.log(storedKelvinNum) //! 298.15 degrees
+// // let storedFahrenheitNum = celsiusToFahrenheit(tempNum); //!WORKS
+// // console.log(storedFahrenheitNum); //! 77 degrees
+
+// //? I have the conversions when dealing with celsius numbers
+// //TODO I NEED a way to tell apart and give me the other degrees
+
+// function tempConverter(currentTempValue, currentTempLetter) {
+//     console.log(currentTempValue + " is the tempNum");
+//     console.log(currentTempLetter + " is the tempUnitLetter");
+//     //MADE THIS VARIABLE just in case a user inputs a lowercase letter.
+//     let upperCaseLetter = currentTempLetter.toUpperCase();
+//     console.log(upperCaseLetter)
+//     let celsius = 0;
+//     let fahrenheit = 0;
+//     let kelvin = 0;
+
+//     if (upperCaseLetter === "C") {
+//         fahrenheit = celsiusToFahrenheit(currentTempValue);
+//         kelvin = celsiusToKelvin(currentTempValue);
+//         celsius = currentTempValue;
+//         console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
+//     } else if (upperCaseLetter === "F") {
+//         fahrenheit = currentTempValue;
+//         celsius = fahrenheitToCelsius(currentTempValue);
+//         kelvin = celsiusToKelvin(celsius);
+//         console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
+//     } else if (upperCaseLetter === "K") {
+//         celsius = KelvinToCelsius(currentTempValue);
+//         fahrenheit = celsiusToFahrenheit(celsius);
+//         kelvin = currentTempValue;
+//         console.log(`Fahrenheit: ${fahrenheit} Kelvin: ${kelvin} Celsius: ${celsius}`);
+//     }
+// }
+
+
+// tempConverter(tempNum, tempUnitLetter)
 
 //? (Hint: There will be two arguements temperature value and unit letter (Celsius, Farenheit, Kelvin))
 //? ex. If I provide 25 and "C" I want the Kelvin and Farenheit temperature
@@ -287,3 +287,73 @@ tempConverter(tempNum, tempUnitLetter)
 
 //TODO  Use if-else Statements: Use if-else statements to determine which conversion to perform based on the user's input.
 //* Output Results: Use console.log to display the converted temperature. 
+
+
+
+
+// Breaking down functions
+
+// function: How to insert data into a array, 
+// function: how to create a object
+// function: how to insert that object into an array
+// 
+
+
+
+
+
+function add() {
+    console.log(2 + 2);
+};
+    
+add();
+add();
+add();
+add();
+
+
+let foo = 18;
+let bar = 45;
+
+function addingTwoNums(leftNumber, rightNumber) {
+    console.log(`${leftNumber} + ${rightNumber} = ${leftNumber + rightNumber}`)
+}
+
+
+addingTwoNums(3, 8);
+addingTwoNums(7, 5);
+addingTwoNums(5, 5);
+addingTwoNums(23, 43);
+addingTwoNums(foo, bar);
+
+
+
+
+function calculateArea(width, height) {
+    //if you need the answer to out of a function
+    //RETURN it
+    let currentWidth = width;
+    console.log(currentWidth);
+    return width * height;
+}
+
+//Will error out
+//console.log(currentWidth)
+
+let areaAnswerOne = calculateArea(12, 3); //36
+let areaAnswerTwo = calculateArea(4, 11); //44
+console.log(areaAnswerOne);
+console.log(areaAnswerTwo);
+
+
+function whichNumberIsHigher(numOne, numTwo) {
+    if (numOne > numTwo) {
+        console.log(`${numOne} is Higher than ${numTwo}`)
+    } else if (numTwo > numOne) {
+        console.log(`${numTwo} is higher than ${numOne}`)
+    } 
+}
+
+whichNumberIsHigher(14, 3);
+// These variables are assigned to functions that RETURN numbers
+whichNumberIsHigher(areaAnswerOne, areaAnswerTwo)
